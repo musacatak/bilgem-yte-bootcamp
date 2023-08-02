86bdc05940658c73193e4ce3751c0dc7fd70fce2
Ahmet Musa Çatak <ahmetmusa.catak@gmail.com>
2023-08-02 16:25:36 +0300

    update

--- React/first-app/src/app/page.tsx
+++ React/first-app/src/app/page.tsx
@@ -2,6 +2,7 @@
 import styles from './page.module.css'
 import FormElement from './components/FormElement'
 import FirstButton from "@/app/components/FirstButton";
+import "./page.css"
 
 import React, {useState} from 'react';
 
@@ -14,11 +15,26 @@ export default function Home() {
 
 
     return (
-        <main>
-            <FormElement myLabel='Name' inputHandler={setName}/>
-            <FormElement myLabel='Company Name' inputHandler={setCompany}/>
-            <FormElement myLabel='Address' inputHandler={setAddress}/>
-            <FirstButton data={[name, company, address]}/>
+        <main className='my-app'>
+            <div className='my-form'>
+                <FormElement myLabel='Name' inputHandler={setName}/>
+                <FormElement myLabel='Company Name' inputHandler={setCompany}/>
+                <FormElement myLabel='Address' inputHandler={setAddress}/>
+                <FirstButton data={[name, company, address]}/>
+
+            </div>
+
+            <div className='person-information'>
+                <h1>Person Information:</h1>
+                <br/>
+                <h2>Name: {name}</h2>
+                <br/>
+                <h2>Company: {company}</h2>
+                <br/>
+                <h2>Address: {address}</h2>
+
+            </div>
+
         </main>
 
     )
