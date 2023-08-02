95268ebe0c8cbb57b7b7863b749ade2df5957a83
Ahmet Musa Çatak <ahmetmusa.catak@gmail.com>
2023-08-02 16:53:33 +0300

    implement type critic

--- React/first-app/src/app/components/FormElement.tsx
+++ React/first-app/src/app/components/FormElement.tsx
@@ -4,9 +4,8 @@ import {Simulate} from "react-dom/test-utils";
 import input = Simulate.input;
 
 interface propsType {
-    myLabel: String,
-    inputHandler?: Dispatch<SetStateAction<any>>
-
+    myLabel: string,
+    inputHandler: Dispatch<string>
 }
 
 export default function FormElement(props: propsType) {
