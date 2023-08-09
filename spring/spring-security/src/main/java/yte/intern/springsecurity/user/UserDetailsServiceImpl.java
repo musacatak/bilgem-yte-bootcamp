01a006b2d54cdfa4d81d0d6cf4a4697a29fd2b76
Ahmet Musa Çatak <ahmetmusa.catak@gmail.com>
2023-08-09 15:04:50 +0300

    throws is added to service

--- spring/spring-security/src/main/java/yte/intern/springsecurity/user/UserDetailsServiceImpl.java
+++ spring/spring-security/src/main/java/yte/intern/springsecurity/user/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package yte.intern.springsecurity.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(final String username){
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
     }
 }
